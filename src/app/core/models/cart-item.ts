e85b89dc45614880670c16774d5d15776defc925
Valentina Krivelskaya <v.krivelskaya@mail.ru>
2021-06-22 10:18:47 +0300

    feat: ordering

--- src/app/core/models/cart-item.ts
+++ src/app/core/models/cart-item.ts
@@ -1,25 +1,25 @@
 import { BookModel } from 'src/app/core/models/book';
 
-export class CartItem {
+export interface CartItemModel {
   book: BookModel;
   count: number;
   total: number;
 
-  constructor(book: BookModel, count: number, total: number) {
-    this.book = book;
-    this.count = count;
-    this.total = total;
-  }
+  // constructor(book: BookModel, count: number, total: number) {
+  //   this.book = book;
+  //   this.count = count;
+  //   this.total = total;
+  // }
 
-  static clone(other: CartItem) {
-    return new CartItem(other.book, other.count, other.total);
-  }
+  // static clone(other: CartItem) {
+  //   return new CartItem(other.book, other.count, other.total);
+  // }
 
-  get bookPrice(): number {
-    return this.book.price;
-  }
+  // get bookPrice(): number {
+  //   return this.book.price;
+  // }
 
-  get bookName(): string {
-    return this.book.name;
-  }
+  // get bookName(): string {
+  //   return this.book.name;
+  // }
 }
